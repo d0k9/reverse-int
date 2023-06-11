d095b701477502bba1e584be87b3a481db7c7602
d0k9 <vdokukin@bigmir.net>
2023-06-11 10:42:42 +0300

    init: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        isNegative = true;
+        n = Math.abs(n);
+    }
+    let reversedString = String(n).split('').reverse().join('');
+    return reversedNumber = Number(reversedString);
 }
